14dd198246da26a7c36b711993eb838c694b714c
alvarobagr <alvaro230604@gmail.com>
2025-10-21 18:43:57 +0200

    adición de maven

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.android.application")
+    //id("com.android.application")
     id("com.google.gms.google-services")
 }
 
@@ -45,4 +45,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.facebook.android:facebook-login:latest.release")
 }
