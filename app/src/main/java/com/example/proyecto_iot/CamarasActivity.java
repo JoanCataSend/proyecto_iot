bb8ba8939b9fecda8d5e2da0d29176674cf7311e
Matilde <matildecalleja103@gmail.com>
2025-11-06 16:04:32 +0100

    Medio arreglao las cams

--- app/src/main/java/com/example/proyecto_iot/CamarasActivity.java
+++ app/src/main/java/com/example/proyecto_iot/CamarasActivity.java
@@ -1,13 +1,21 @@
 package com.example.proyecto_iot;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CamarasActivity extends AppCompatActivity {
+    private ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camaras);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        // --- VOLVER ---
+        btnBack.setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/example/proyecto_iot/MainActivity.java
+++ app/src/main/java/com/example/proyecto_iot/MainActivity.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,7 +16,7 @@ public class MainActivity extends NavBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.intento);
+        setContentView(R.layout.main);
 
         inicializarNavbar(R.id.nav_home);
 
